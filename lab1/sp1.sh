8ccc92f88f3355b3e8773ec96b10297bdbbe24c3
Rodion Antipov <300microgram@gmail.com>
2021-03-04 18:18:12 +0700

    Initial commit

---
+++ lab1/sp1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "Создание образа CD-диска"
+echo "С помощью данного скрипта пользователь может создать образ CD-диска на основе выбранного каталога"
+echo "Разработчик: Антипов Родион 727-1"
+while true
+do
+	while true
+	do
+		echo "Введите путь к каталогу:"
+		read dirPath
+		if [ -d $dirPath ]
+		then
+			break
+		else
+			echo "Каталог не найден!"
+		fi
+	done
+	echo "Введите название образа:"
+	read imageName
+	if [ -f $imageName.iso ]
+	then
+		echo "Файл $imageName.iso уже существует, образ будет назван $imageName$(date +%Y-%m-%d-%H-%M).iso"
+		imageName="$imageName$(date +%Y-%m-%d-%H-%M)"
+	fi
+	imageName="$imageName.iso"
+	if genisoimage -o $imageName $dirPath
+	then
+		echo "Образ создан"
+	else
+		echo "При создании образа возникла ошибка"
+	fi
+	echo "Образы в корневой папке:"
+	ls *.iso
+	echo "Содержимое каталога, на основе которого был создан образ:"
+	ls $dirPath
+	echo "Содержимое образа:"
+	7z l $imageName
+	echo "Завершить выполнения скрипта (y/n)"
+	read reply
+	if [ $reply = "y" ]
+	then
+		break
+	fi
+done
